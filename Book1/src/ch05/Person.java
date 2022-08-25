df67fc12dc1dc12b9883e3b9c408284d6f9c73e4
bskim617 <bskim617@gmail.com>
2022-08-25 13:50:13 +0900

    2022/08/25

---
+++ Book1/src/ch05/Person.java
@@ -0,0 +1,13 @@
+package ch05;
+
+//p131
+
+public class Person {
+	
+	String name;
+	int height;
+	double weight;
+	char gender;
+	boolean married;
+
+}
---
+++ Book1/src/ch05/Student.java
@@ -0,0 +1,18 @@
+package ch05;
+
+// p129
+
+public class Student {
+	
+	int studentID;
+	String studentName;
+	int grade;
+	String address;
+	
+	// p132
+	public void show() {
+		System.out.println(studentName + " , " + address );
+		
+	}
+	
+}
